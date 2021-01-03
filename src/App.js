b90821d21fddc7daa879c1521f3c24f761cda775
Maxim Dubrovin <max28101998@gmail.com>
2021-01-03 14:35:46 +0300

    added layout managment and created title

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Navs from './components/Navs'
 import Home from './pages/Home'
 import Starred from './pages/Starred'
 
 function App() {
   return (
-    <div className="">
-      <Navs />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-
-        <Route>
-          <div>Not found</div>
-        </Route>
-      </Switch>
-    </div>
+      <Route>
+        <div>Not found</div>
+      </Route>
+    </Switch>
   )
 }
 
