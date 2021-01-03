550a310ce3d26147149d867a1ae4c453f1ceee1b
Maxim Dubrovin <max28101998@gmail.com>
2021-01-03 21:54:35 +0300

    created two new more hooks: useLastQuery and useShow

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,5 +1,6 @@
-import { useEffect, useReducer } from 'react'
-import { add, remove } from './constant'
+import { useEffect, useReducer, useState } from 'react'
+import { apiGet } from './config'
+import { add, remove, fetchFailed, fetchSuccess } from './constant'
 
 function showsReducer(prevState, action) {
   switch (action.type) {
@@ -29,4 +30,56 @@ function usePersistedReducer(reducer, initialState, key) {
 
 export function useShows(key = 'shows') {
   return usePersistedReducer(showsReducer, [], key)
+}
+
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key)
+    return persisted ? JSON.parse(persisted) : ''
+  })
+
+  const setPersistedInput = newState => {
+    setInput(newState)
+    sessionStorage.setItem(key, JSON.stringify(newState))
+  }
+
+  return [input, setPersistedInput]
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case fetchSuccess:
+      return { ...prevState, isLoading: false, show: action.show }
+    case fetchFailed:
+      return { ...prevState, isLoading: false, error: action.error }
+    default:
+      return prevState
+  }
+}
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  })
+
+  useEffect(() => {
+    let isMounted = true
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: fetchSuccess, show: results })
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: fetchFailed, error: err.message })
+        }
+      })
+    return () => (isMounted = false)
+  }, [showId])
+
+  return state
 }
